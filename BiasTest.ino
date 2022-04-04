d505c1c84ab55ed0f8e73e519663ef4b420f5f3e
Kendra <44911612+Kendra17@users.noreply.github.com>
2022-04-04 12:22:30 -0500

    TESTING - Fixed Bias Test
    Changed to make file names consistent throughout program

--- BiasTest.ino
+++ BiasTest.ino
@@ -1,3 +1,11 @@
+#include <BufferedPrint.h>
+#include <FreeStack.h>
+#include <MinimumSerial.h>
+#include <RingBuf.h>
+#include <SdFat.h>
+#include <SdFatConfig.h>
+#include <sdios.h>
+
 #include <SPI.h>
 #include "SdFat.h"
 #include "sdios.h"
@@ -9,7 +17,7 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-using namespace BLA;
+//using namespace BLA;
 
 Adafruit_BNO055 bno = Adafruit_BNO055(01, 0x28);
 imu::Vector<3> accldata, gyrodata;
@@ -20,8 +28,8 @@ SdFile myFile;
 // change this to match your SD shield or module;
 const int chipSelect = 10;
 bool detectError;
-bool landed;
-int loops = 338;
+bool landed = false;
+int loops = 0;
 char line[10];
 bool transmit = false;
 int timestamptracker = 0;
@@ -38,7 +46,7 @@ void setup()
   sd.begin(chipSelect, SPI_HALF_SPEED);
   Serial.println("initialization done.");
 
-  myFile.open("FullScale.csv", O_RDWR | O_CREAT | O_AT_END);
+  myFile.open("With_battery.csv", O_RDWR | O_CREAT | O_AT_END);
   myFile.println("Acceleration/Gyroscope");
   myFile.close();
   detectError = bno.begin();
@@ -48,21 +56,20 @@ void setup()
     detectError = bno.begin();
     /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("No BNO055 detected");
-    myFile.open("FullScale.csv", O_RDWR | O_AT_END);
+    myFile.open("With_battery.csv", O_RDWR | O_AT_END);
     myFile.println("No BNO055 detected");
     myFile.close();
     headercount++;
     delay(100);
   }
-  landed = true;
-  curtime[0] = 0;
+  //landed = true;
 }
 
 void loop()
 {
   if(landed == false)
   {
-    myFile.open("FullScale.csv", O_RDWR | O_AT_END);
+    myFile.open("With_battery.csv", O_RDWR | O_AT_END);
     myFile.println(millis());
     for(int x = 0; x < 1000; x++)
     {
